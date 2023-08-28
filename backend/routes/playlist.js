257c63065fb38811420be3aa3a88ffdab36112c1
Dhruv Dhari <dhruvdhari2003@gmail.com>
2023-08-28 17:56:47 +0530

    updated

--- backend/routes/playlist.js
+++ backend/routes/playlist.js
@@ -20,6 +20,12 @@ router.post("/create",passport.authenticate("jwt",{session:false}),async(req,res
 
 router.get("/get/:playlistId",passport.authenticate("jwt",{session:false}),async(req,res)=>{
     const playlistId=req.params.playlistId;
+    const playlist =await Playlist.findOne({_id :playlistId});
+
+    if(!playlist){
+        return res.status(301).json({err:"Invalid ID"});
+    }
+    return res.status(200).json(playlist)
 });
 
 module.exports = router;
