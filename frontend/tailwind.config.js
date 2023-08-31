4c1fbadf365f51ab6559f5f92b413eaa0caa9226
Dhruv Dhari <dhruvdhari2003@gmail.com>
2023-09-01 01:37:34 +0530

    figuring out how to add customized css to tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,10 +2,11 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    fontFamily:{
-      "poppins":"Poppins , sans-serif"
-    },
+   
     extend: {
+      fontFamily:{
+        poppins:["Poppins" , "sans-serif"]
+      }
      
     },
   },
