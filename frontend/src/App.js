13fa0dd02a40e072a700dbd93f93e778bbcbe33a
Dhruv Dhari <dhruvdhari2003@gmail.com>
2023-08-31 17:48:41 +0530

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import {BrowserRouter,Routes,Route} from "react-router-dom";
 
 
 function App() {
   return (
     <>
+  
+    <div className="App">
+      
+    <BrowserRouter>
+    </BrowserRouter>
     
+
+    </div>
     
     </>
    
