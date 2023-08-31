e95e686064945c9c5563fb91459fd86fc598388a
Dhruv Dhari <dhruvdhari2003@gmail.com>
2023-08-31 20:13:27 +0530

    added spotify icon to the login page

--- frontend/src/routes/Login.js
+++ frontend/src/routes/Login.js
@@ -1,8 +1,20 @@
+import { Icon } from '@iconify/react';
+
 const LoginComponent=()=>{
     return(
-        <>
-        <div className="w-full h-full">login</div>
-        </>
+      
+        <div className="w-full h-full flex flex-col items-center">
+            <div className="logo p-5 border-b border-solid border-gray-300 w-full flex justify-center">
+            <Icon icon="logos:spotify" width="150" />
+            </div>
+            <div className="inputRegion">
+                {/* isme email and password aaega and login button */}
+            </div>
+       
+        
+        
+        </div>
+       
     );
 };
 
