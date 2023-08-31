6f18dfc92b5df67bc4c15374ce63a8cfaa4badd8
Dhruv Dhari <dhruvdhari2003@gmail.com>
2023-08-31 22:57:38 +0530

    mostly done with the login page

--- frontend/src/components/shared/Textinput.js
+++ frontend/src/components/shared/Textinput.js
@@ -1,8 +1,8 @@
-const TextInput=({label,placeholder})=>{
+const TextInput=({label,placeholder,className})=>{
     return(
-        <div className="textInputDiv flex flex-col space-y-2 w-full">
+        <div className={`textInputDiv flex flex-col space-y-2 w-full ${className}`}>
             <label for={label} className="font-semibold" >{label}</label>
-               <input type="text" placeholder={placeholder} className="p-2 border-1 border-solid border-gray-300 rounded placeholder-gray-500 "
+               <input type="text" placeholder={placeholder} className="p-3 border-1 border-solid border-gray-300 rounded placeholder-gray-500 "
                id={label}
                 />
 
