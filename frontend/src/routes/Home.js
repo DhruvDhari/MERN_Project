7e8a10e2cec3e377e0cf811bef471b1b813cbc71
Dhruv Dhari <dhruvdhari2003@gmail.com>
2023-09-01 17:42:38 +0530

    done with signup

---
+++ frontend/src/routes/Home.js
@@ -0,0 +1,16 @@
+const HomeComponent=()=>{
+    return(
+        <>
+        <div className="h-full w-full flex">
+            <div className="left h-full w-1/5 bg-black">
+
+            </div>
+
+            <div className="right h-full">
+
+            </div>
+        </div>
+        </>
+    );
+}
+export default HomeComponent;
