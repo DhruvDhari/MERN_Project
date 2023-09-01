5dbb5823a8c4afff463dc83c8d6ad452cfb5a6cd
Dhruv Dhari <dhruvdhari2003@gmail.com>
2023-09-01 19:47:48 +0530

    done with sidebar

--- frontend/src/components/shared/IconText.js
+++ frontend/src/components/shared/IconText.js
@@ -5,7 +5,7 @@ const IconText=({iconName,displayText,active})=>{
         <div className="flex items-center justify-start cursor-pointer ">
 
             <div className="py-2 px-3">
-                <Icon icon={iconName} color={active?"white":"gray"} fontSize={35}/>
+                <Icon icon={iconName} color={active?"white":"gray"} fontSize={27}/>
             </div>
             <div className={`${active?"text-white":"text-gray-400"} text-sm font-semibold hover:text-white`} >
                 {displayText}
